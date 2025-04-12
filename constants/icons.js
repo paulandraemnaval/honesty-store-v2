59a2224ce5174e299386b9fb4ade87b6f7f2294a
PaulNaval <22-1-00759@vsu.edu.ph>
2025-04-12 12:09:08 +0800

    added metadata

--- constants/icons.js
+++ constants/icons.js
@@ -1,19 +1,11 @@
-import homeIcon from "/icons/home_icon.png";
-import productsIcon from "/icons/products_icon.png";
-import manageIcon from "/icons/manage_icon.png";
-import accountsIcon from "/icons/accounts_icon.png";
-import logo from "/metaIcons/tab_icon.png";
-import auditIcon from "/icons/audit_icon.png";
-import createAccount from "/icons/create_account.png";
-
 const icons = {
-  homeIcon,
-  productsIcon,
-  manageIcon,
-  logo,
-  auditIcon,
-  createAccount,
-  accountsIcon,
+  homeIcon: "/icons/home_icon.png",
+  productsIcon: "/icons/products_icon.png",
+  manageIcon: "/icons/manage_icon.png",
+  logo: "/metaIcons/tab_icon.png",
+  auditIcon: "/icons/audit_icon.png",
+  createAccount: "/icons/create_account.png",
+  accountsIcon: "/icons/accounts_icon.png",
 };
 
 export default icons;
